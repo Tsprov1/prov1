fbb425b071d01d821ef9a93009c6aa7f1cfc432b
tspro <tiensipt123@gmail.com>
2025-10-04 14:21:58 +0700

    thoat đây là cái git có cái nịt nha :wq

---
+++ pro.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    for (int i = 0; i < 10; i++) {
+        printf("Hello, World!\n");
+    }
+    return 0;
+
+}
+// This is a simple C program that prints "Hello, World!" ten times.
